b5bc6d95fbd3a9e44e1ff958974c785d123561ca
ThiagoRM09 <trthiago2009@gmail.com>
2026-06-26 19:06:31 -0300

    feat: classes tests on Main class

--- src/main/java/com/app/controller/Main.java
+++ src/main/java/com/app/controller/Main.java
@@ -4,7 +4,29 @@ import com.app.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        Funcionario f = new Funcionario();
-        System.out.println(f.toString());
+
+        Candidato candidato = new Candidato();
+        System.out.println(candidato.toString());
+
+        Candidatura candidatura = new Candidatura();
+        System.out.println(candidatura.toString());
+
+        Contrato contrato = new Contrato();
+        System.out.println(contrato.toString());
+
+        DadosBancarios dadosBancarios = new DadosBancarios();
+        System.out.println(dadosBancarios.toString());
+
+        Departamento departamento = new Departamento();
+        System.out.println(departamento.toString());
+
+        FolhaDePagamento folhaDePagamento = new FolhaDePagamento();
+        System.out.println(folhaDePagamento.toString());
+
+        Funcionario funcionario = new Funcionario();
+        System.out.println(funcionario.toString());
+
+        Vaga vaga = new Vaga();
+        System.out.println(vaga.toString());
     }
 }
