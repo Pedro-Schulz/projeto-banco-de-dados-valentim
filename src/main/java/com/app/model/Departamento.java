ccfc8947c68311466f5793b1190869c59b8603e1
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-06-26 18:36:33 -0300

    feat: model class 'Departamento' added

---
+++ src/main/java/com/app/model/Departamento.java
@@ -0,0 +1,57 @@
+package com.app.model;
+
+public class Departamento {
+    private Integer idDepartamento;
+    private String nome;
+    private Double gastos;
+    private Double retorno;
+
+    public Departamento() {}
+
+    public Departamento(Integer idDepartamento, String nome, Double gastos, Double retorno) {
+        this.idDepartamento = idDepartamento;
+        this.nome = nome;
+        this.gastos = gastos;
+        this.retorno = retorno;
+    }
+
+    public Integer getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Integer idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(Double gastos) {
+        this.gastos = gastos;
+    }
+
+    public Double getRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(Double retorno) {
+        this.retorno = retorno;
+    }
+
+    @Override
+    public String toString() {
+        return "\n> ID: " + idDepartamento +
+                "\n> NOME: " + nome +
+                "\n> GASTOS: " + gastos +
+                "\n> RETORNO FINANCEIRO: " + retorno;
+    }
+}
