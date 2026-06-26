5564762abf1d837a5aa2cc09650bae0e365417aa
ThiagoRM09 <trthiago2009@gmail.com>
2026-06-26 18:22:01 -0300

    feat: model class 'Contrato' added

---
+++ src/main/java/com/app/model/Contrato.java
@@ -0,0 +1,51 @@
+package com.app.model;
+
+import java.time.LocalDate;
+
+public class Contrato {
+    private Integer idContrato;
+    private Boolean statusContrato;
+    private LocalDate dataEmissao;
+    private Integer prazo;
+
+    public Contrato() {};
+
+    public Contrato(Integer idContrato, Boolean statusContrato, LocalDate dataEmissao, Integer prazo) {
+        this.idContrato = idContrato;
+        this.statusContrato = statusContrato;
+        this.dataEmissao = dataEmissao;
+        this.prazo = prazo;
+    }
+
+    public Integer getIdContrato() {
+        return idContrato;
+    }
+
+    public void setIdContrato(Integer idContrato) {
+        this.idContrato = idContrato;
+    }
+
+    public Boolean getStatusContrato() {
+        return statusContrato;
+    }
+
+    public void setStatusContrato(Boolean statusContrato) {
+        this.statusContrato = statusContrato;
+    }
+
+    public LocalDate getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(LocalDate dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+
+    public Integer getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(Integer prazo) {
+        this.prazo = prazo;
+    }
+}
