29cd9e737eef58e98187a356c398f1c523c6b8b5
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-06-29 19:29:05 -0300

    test: connection in main

--- src/main/java/com/app/controller/Main.java
+++ src/main/java/com/app/controller/Main.java
@@ -1,32 +1,14 @@
 package com.app.controller;
 
-import com.app.model.*;
+import com.app.*;
+import com.app.config.ConnectionFactory;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
 
-        Candidato candidato = new Candidato();
-        System.out.println(candidato.toString());
-
-        Candidatura candidatura = new Candidatura();
-        System.out.println(candidatura.toString());
-
-        Contrato contrato = new Contrato();
-        System.out.println(contrato.toString());
-
-        DadosBancarios dadosBancarios = new DadosBancarios();
-        System.out.println(dadosBancarios.toString());
-
-        Departamento departamento = new Departamento();
-        System.out.println(departamento.toString());
-
-        FolhaDePagamento folhaDePagamento = new FolhaDePagamento();
-        System.out.println(folhaDePagamento.toString());
-
-        Funcionario funcionario = new Funcionario();
-        System.out.println(funcionario.toString());
-
-        Vaga vaga = new Vaga();
-        System.out.println(vaga.toString());
+        Connection c = ConnectionFactory.getConnection();
+        System.out.println("Conectado!");
     }
 }
