72da573cfec2397897534211b037acab110c309f
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-07-01 18:42:36 -0300

    feat: method 'buscarPorId' added in the class 'DepartamentoRepository'

--- src/main/java/com/app/controller/Main.java
+++ src/main/java/com/app/controller/Main.java
@@ -6,7 +6,7 @@ import com.app.repository.*;
 public class Main {
     public static void main(String[] args) {
 
-        Vaga v = VagaRepository.buscarPorId(1L);
-        System.out.println(v.toString());
+        Departamento departamento = DepartamentoRepository.buscarPorId(1L);
+        System.out.println(departamento.toString());
     }
 }
--- src/main/java/com/app/repository/DepartamentoRepository.java
+++ src/main/java/com/app/repository/DepartamentoRepository.java
@@ -5,6 +5,7 @@ import com.app.model.Departamento;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DepartamentoRepository {
@@ -30,6 +31,33 @@ public class DepartamentoRepository {
     }
 
     public static Departamento buscarPorId(Long id) {
+        String sql = """
+            SELECT *
+            FROM departamentos
+            WHERE id_departamento = ?;
+        """;
+
+        try {
+            Connection c = ConnectionFactory.getConnection();
+            PreparedStatement p = c.prepareStatement(sql);
+
+            p.setLong(1, id);
+
+            ResultSet rs = p.executeQuery();
+
+            if(rs.next()) {
+                Departamento departamento = new Departamento(
+                    rs.getLong("id_departamento"),
+                    rs.getString("nome"),
+                    rs.getDouble("gastos"),
+                    rs.getDouble("retorno")
+                );
+
+                return departamento;
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("Erro ao buscar o departamento pelo ID!");
+        }
         return null;
     }
 }
