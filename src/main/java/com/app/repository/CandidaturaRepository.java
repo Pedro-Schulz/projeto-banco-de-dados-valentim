f0bcc81e3a41f37d08a837d177453a1eab507842
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-07-06 15:28:52 -0300

    feat: 'vinculoVaga' added in CandidaturaRepository

--- src/main/java/com/app/repository/CandidaturaRepository.java
+++ src/main/java/com/app/repository/CandidaturaRepository.java
@@ -1,4 +1,31 @@
 package com.app.repository;
 
+import com.app.config.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class CandidaturaRepository {
+    public boolean vinculoVaga(Long id_vaga) throws RuntimeException {
+        String sql = """
+            SELECT 1
+            FROM candidaturas
+            WHERE id_vaga = ?
+            LIMIT 1;
+        """;
+
+        try (
+                Connection c = ConnectionFactory.getConnection();
+                PreparedStatement p = c.prepareStatement(sql);
+        ) {
+            p.setLong(1, id_vaga);
+
+            ResultSet rs = p.executeQuery();
+
+            return rs.next();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao verificar vínculo candidatura -> vaga", e);
+        }
+    }
 }
