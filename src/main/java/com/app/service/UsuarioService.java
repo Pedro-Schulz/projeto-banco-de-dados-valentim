2a0c35e12fba5f5d26cbbcce0aafb621b00125f7
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-07-16 20:36:46 -0300

    feat: usuario service created

---
+++ src/main/java/com/app/service/UsuarioService.java
@@ -0,0 +1,21 @@
+package com.app.service;
+
+import com.app.repository.UsuarioRepository;
+import com.app.model.Usuario;
+
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void salvar(Usuario usuario) {
+        validarSenha(usuario.getSenha());
+        usuarioRepository.salvar(usuario);
+    }
+
+    public boolean validarSenha(String senha) {
+        return true;
+    }
+}
