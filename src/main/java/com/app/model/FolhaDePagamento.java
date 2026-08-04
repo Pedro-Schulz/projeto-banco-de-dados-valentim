25481b8d9c3d13aa7ebdb2322b5fdb6abaf8638a
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-08-03 23:56:09 -0300

    feat: application of the Hibernate library to the FolhaDePagamento class

--- src/main/java/com/app/model/FolhaDePagamento.java
+++ src/main/java/com/app/model/FolhaDePagamento.java
@@ -1,5 +1,8 @@
 package com.app.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,12 +15,29 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class FolhaDePagamento {
     private Long idFolha;
+
+    @NotNull(message = "As horas trabalhadas são obrigatórias")
+    @PositiveOrZero(message = "As horas trabalhadas não podem ser negativas")
     private Integer horasTrabalhadas;
+
+    @NotNull(message = "A data de emissão é obrigatória")
+    @PastOrPresent(message = "A data de emissão não pode ser futura")
     private LocalDate dataEmissao;
+
+    @NotNull(message = "Os descontos são obrigatórios")
+    @PositiveOrZero(message = "Os descontos não podem ser negativos")
     private Double descontos;
+
+    @NotNull(message = "As horas extras são obrigatórias")
+    @PositiveOrZero(message = "As horas extras não podem ser negativas")
     private Integer horasExtras;
+
+    @NotNull(message = "O funcionário é obrigatório")
     private Funcionario funcionario;
+
+    @NotNull(message = "O status de ativo é obrigatório")
     private Boolean ativo;
+
     private Integer version = 1;
 
     public FolhaDePagamento(Integer horasTrabalhadas, LocalDate dataEmissao, Double descontos, Integer horasExtras, Funcionario funcionario, Boolean ativo) {
