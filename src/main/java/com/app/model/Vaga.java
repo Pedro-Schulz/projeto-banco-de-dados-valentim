68b4301984700e0b2ba214085d9954b70b1e8237
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-08-04 00:02:20 -0300

    feat: application of the Hibernate library to the Vaga class

--- src/main/java/com/app/model/Vaga.java
+++ src/main/java/com/app/model/Vaga.java
@@ -1,5 +1,9 @@
 package com.app.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +15,25 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Vaga {
     private Long idVaga;
+
+    @NotBlank(message = "O turno é obrigatório")
+    @Size(max = 30, message = "O turno deve possuir no máximo 30 caracteres")
     private String turno;
+
+    @NotBlank(message = "O cargo é obrigatório")
+    @Size(max = 100, message = "O cargo deve possuir no máximo 100 caracteres")
     private String cargo;
+
+    @NotNull(message = "O salário por hora é obrigatório")
+    @Positive(message = "O salário por hora deve ser maior que zero")
     private Double salarioHora;
+
+    @NotNull(message = "O departamento é obrigatório")
     private Departamento departamento;
+
+    @NotNull(message = "O status de ativo é obrigatório")
     private Boolean ativo;
+
     private Integer version = 1;
 
     public Vaga(Long idVaga) {
